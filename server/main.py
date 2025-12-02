11eef7b43c0d343f7cd6603a0903a38c53e0a840
chibuike anyiam <anyiamchibuike3@gmail.com>
2025-12-02 13:06:07 -0500

    updated client with dashboard adjustments and integrated new API endpoints in backend
    # Conflicts:
    #	server/venv/lib/python3.9/site-packages/click/decorators.py
    #	server/venv/lib/python3.9/site-packages/flask/cli.py
    #	server/venv/lib/python3.9/site-packages/pip/_vendor/distlib/util.py
    #	server/venv/lib/python3.9/site-packages/pip/_vendor/pygments/lexer.py
    #	server/venv/lib/python3.9/site-packages/pip/_vendor/requests/_internal_utils.py
    #	server/venv/lib/python3.9/site-packages/pip/_vendor/requests/utils.py
    #	server/venv/lib/python3.9/site-packages/pip/_vendor/rich/progress.py
    #	server/venv/lib/python3.9/site-packages/setuptools/_distutils/archive_util.py
    #	server/venv/lib/python3.9/site-packages/setuptools/_distutils/ccompiler.py
    #	server/venv/lib/python3.9/site-packages/setuptools/_distutils/filelist.py
    #	server/venv/lib/python3.9/site-packages/setuptools/_distutils/util.py
    #	server/venv/lib/python3.9/site-packages/setuptools/command/bdist_egg.py
    #	server/venv/lib/python3.9/site-packages/setuptools/glob.py

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 import os
 
-import dotenv
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from dotenv import load_dotenv
+from bson import ObjectId
 
 load_dotenv()
 
@@ -15,9 +15,38 @@ db = mongo.db
 
 cors = CORS(app, origins='*')
 
-@app.route("/api/db", methods=['GET'])
-def user():
-    userName= db.users.find_one()
+@app.route("/Users", methods=['GET'])
+def get_users():
+    data = list(db.users.find())
+
+    for users in data:
+        users["_id"] = str(users["_id"])
+
+    return jsonify(data), 200
+
+@app.route("/Users/<user_name>", methods=['GET'])
+def get_user_by_id(user_name):
+    user = db.users.find_one({"user_name": user_name})
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    user["_id"] = str(user["_id"])
+    return jsonify(user), 200
+
+@app.route("/Users/<user_name>/<date>", methods=['DELETE'])
+def delete_user_details_by_date(user_name, date):
+    db.users.update_one({"user_name": user_name},{"$unset": {date: ""}})
+    return jsonify({"message": "date delete successfully"}), 200
+
+
+
+
+# get information for a single user
+#
+# get information on all users for the dashboard
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
