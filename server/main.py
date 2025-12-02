df3865b19c471b2ae54387a3281316a591466375
chibuike anyiam <anyiamchibuike3@gmail.com>
2025-12-02 13:06:07 -0500

    Refactor server to improve API response, add `.env` to `.gitignore`, and update project configuration.

--- server/main.py
+++ server/main.py
@@ -4,7 +4,6 @@ from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from dotenv import load_dotenv
-from bson import ObjectId
 
 load_dotenv()
 
@@ -15,38 +14,14 @@ db = mongo.db
 
 cors = CORS(app, origins='*')
 
-@app.route("/Users", methods=['GET'])
-def get_users():
+@app.route("/api/db", methods=['GET'])
+def user():
     data = list(db.users.find())
 
-    for users in data:
-        users["_id"] = str(users["_id"])
+    for u in data:
+        u["_id"] = str(u["_id"])
 
     return jsonify(data), 200
 
-@app.route("/Users/<user_name>", methods=['GET'])
-def get_user_by_id(user_name):
-    user = db.users.find_one({"user_name": user_name})
-
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    user["_id"] = str(user["_id"])
-    return jsonify(user), 200
-
-@app.route("/Users/<user_name>/<date>", methods=['DELETE'])
-def delete_user_details_by_date(user_name, date):
-    db.users.update_one({"user_name": user_name},{"$unset": {date: ""}})
-    return jsonify({"message": "date delete successfully"}), 200
-
-
-
-
-# get information for a single user
-#
-# get information on all users for the dashboard
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    app.run(debug=True, port=8080)
