a9910a0b9b8ad7e77d49cbd1e186a6d7fa978b86
Chibuike Anyiam <anyiamchibuike3@gmail.com>
2026-06-02 20:18:47 -0400

    Update client to use environment-based API URL, add `@types/node` dependency, and update hooks for dynamic endpoints.

--- client/src/hooks/useUsers.ts
+++ client/src/hooks/useUsers.ts
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 
 export function useUsers() {
+    const baseUrl = import.meta.env.VITE_API_URL;
 
     const [usersData, setUsersData] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
@@ -8,7 +9,7 @@ export function useUsers() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch("http://127.0.0.1:8080/api/users");
+                const response = await fetch(`${baseUrl}/api/users`);
                 if (!response.ok) throw new Error("Failed to fetch database");
 
                 const jsonData = await response.json();
@@ -22,6 +23,8 @@ export function useUsers() {
 
         fetchData();
     }, []);
+
+    console.log(usersData[0]);
 
 
     return { usersData, loading} ;
