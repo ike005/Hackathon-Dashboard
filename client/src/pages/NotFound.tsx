4d425dc039e3b1fff2df0b66d49a008de3b8e382
Chibuike Anyiam <anyiamchibuike3@gmail.com>
2026-06-03 00:02:06 -0400

    Add missing space in "404" header text of NotFound page.

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
 
     return (
         <div className="flex flex-col justify-center items-center h-screen w-full">
-            <h1 className="text-[#145BEC] text-6xl font-bold">404 </h1>
+            <h1 className="text-[#145BEC] text-6xl font-bold">404</h1>
         </div>
     );
 }
